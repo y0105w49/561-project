886a070489ad15d4e3dc525197f196b5ea5a1d1f
Kunal Mittal <kunal@dynamic-oit-vapornet100-10-8-3-240.princeton.edu>
2020-12-08 10:06:02 -0500

    data output

--- python/evaluate.py
+++ python/evaluate.py
@@ -1,21 +1,25 @@
 from query_process import *
 
 list_gammas = [2**(-i) for i in range(10)]
-T_q = 10
+T_q = 1000
 
 
 # Simulations for a single query :
 
-for b in range(1):
+for b in range(2):
 	for ppow in range(2):
 		filename = "Beaucoup"+str(b)+str(ppow)+"_errors.txt"
 		f = open(filename, "w+")
+		ite = 0
 		for gamma in list_gammas:
 			curr_best_config, bias, rel_error, l2_rel_error = find_best_config(T_q, gamma, b, ppow)
 			ind = b + 2*ppow
 			arr = [gamma, rel_error, l2_rel_error, bias]+curr_best_config
 			strtoprint = ",".join(str(x) for x in arr)
 			f.write(strtoprint+"\n")
+			
+			ite += 1
+			print(ite)
 		f.close()
 
 
