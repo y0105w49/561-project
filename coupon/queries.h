8050ffe13aa87f53257959b5497277f113944510
Antonio J. Molina Lovett <antonio@amolina.ca>
2020-12-08 03:44:58 -0500

    alert both key and queryNum
    doesn't actually display key.  tbh, not sure why that python still
    works, just assuming it's being passed an int16_t, even when being
    passed the full table_key_t.  i guess the queryNum comes first in memory
    probably?
    
    also make example trigger a bit more.

--- coupon/queries.h
+++ coupon/queries.h
@@ -3,7 +3,8 @@
   if (false) {
   } else if (0 <= h && h <= 4026531839) {
     masked.srcIP = pkt->srcIP; masked.dstIP = 0; masked.srcPort = 0; masked.dstPort = 0; masked.timestamp = 0;
-    queueCollection(ctx, 1, 20, (h-0) >> 27, &masked);
+    key.queryNum = 1;
+    queueCollection(ctx, 20, (h-0) >> 27, &key);
   }
   
   masked.srcIP = pkt->srcIP; masked.dstIP = 0; masked.srcPort = 0; masked.dstPort = 0; masked.timestamp = 0;
@@ -11,15 +12,17 @@
   if (false) {
   } else if (0 <= h && h <= 2013265919) {
     masked.srcIP = 0; masked.dstIP = pkt->dstIP; masked.srcPort = 0; masked.dstPort = 0; masked.timestamp = 0;
-    queueCollection(ctx, 2, 20, (h-0) >> 26, &masked);
+    key.queryNum = 2;
+    queueCollection(ctx, 20, (h-0) >> 26, &key);
   }
   
   masked.srcIP = 0; masked.dstIP = 0; masked.srcPort = 0; masked.dstPort = pkt->dstPort; masked.timestamp = 0;
   h = hash(&masked);
   if (false) {
-  } else if (0 <= h && h <= 1342177279) {
+  } else if (0 <= h && h <= 1073741823) {
     masked.srcIP = pkt->srcIP; masked.dstIP = pkt->dstIP; masked.srcPort = 0; masked.dstPort = 0; masked.timestamp = 0;
-    queueCollection(ctx, 3, 10, (h-0) >> 27, &masked);
+    key.queryNum = 3;
+    queueCollection(ctx, 2, (h-0) >> 28, &key);
   }
   
   masked.srcIP = 0; masked.dstIP = 0; masked.srcPort = 0; masked.dstPort = 0; masked.timestamp = pkt->timestamp;
@@ -27,10 +30,12 @@
   if (false) {
   } else if (0 <= h && h <= 805306367) {
     masked.srcIP = pkt->srcIP; masked.dstIP = pkt->dstIP; masked.srcPort = 0; masked.dstPort = 0; masked.timestamp = 0;
-    queueCollection(ctx, 4, 3, (h-0) >> 28, &masked);
-  } else if (805306368 <= h && h <= 1879048191) {
+    key.queryNum = 4;
+    queueCollection(ctx, 3, (h-0) >> 28, &key);
+  } else if (805306368 <= h && h <= 2952790015) {
     masked.srcIP = pkt->srcIP; masked.dstIP = pkt->dstIP; masked.srcPort = pkt->srcPort; masked.dstPort = pkt->dstPort; masked.timestamp = 0;
-    queueCollection(ctx, 5, 6, (h-805306368) >> 27, &masked);
+    key.queryNum = 5;
+    queueCollection(ctx, 6, (h-805306368) >> 28, &key);
   }
   
   masked.srcIP = pkt->srcIP; masked.dstIP = 0; masked.srcPort = pkt->srcPort; masked.dstPort = 0; masked.timestamp = 0;
@@ -38,6 +43,7 @@
   if (false) {
   } else if (0 <= h && h <= 1073741823) {
     masked.srcIP = 0; masked.dstIP = pkt->dstIP; masked.srcPort = 0; masked.dstPort = pkt->dstPort; masked.timestamp = 0;
-    queueCollection(ctx, 6, 6, (h-0) >> 27, &masked);
+    key.queryNum = 6;
+    queueCollection(ctx, 6, (h-0) >> 27, &key);
   }
   
