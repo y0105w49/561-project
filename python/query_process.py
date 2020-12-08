4e7a0bdbeec75896fbefb55626e3945601308904
Kunal Mittal <kunal@dynamic-oit-vapornet100-10-8-3-240.princeton.edu>
2020-12-07 20:02:16 -0500

    add experiment and two metrics in query_process

--- python/query_process.py
+++ python/query_process.py
@@ -19,35 +19,79 @@ import math
 # variables
 MAX_MEMORY = 32 			# w = 32-bit
 ERROR_TOLERANCE = 0.05
-# NUM_TRIALS = 10			# for find_best_config_new
-P_LIMIT = 20
+P_LIMIT = 30
+NUM_TRIALS = 50			# for find_best_config_new
 
-def expected_CC(m,inv_p,n):
-	ret = 0
-	for j in range(n):
-		ret += inv_p/(m-j)
+# order = 1 gives expectation, order = 2 gives squared variance
+def moment_CC(m, inv_p, order):
+	ret = [0]*m
+	for j in range(m):
+		val = inv_p/(m-j)
+		if order == 2:
+			val = val*val - val
+		if j == 0:
+			ret[j] = val
+		else:
+			ret[j] = ret[j-1] + val
 	return ret
-
-
-# def CC_experiment(m,inv_p, n, T):
-# 	collected = [0]*m
-# 	num_collected = 0
-# 	for it in range(1,3*T+1):				# 3 is arbitrary
-# 		val = random.uniform(0,1)
-# 		coupon = math.floor(val*inv_p)
-# 		if coupon<m and collected[coupon] == 0:
-# 			collected[coupon] = 1
-# 			num_collected += 1
-# 			if num_collected == m:
-# 				return it
-# 	return 3*T_q
 
 def relative_error(T, T_q):
 	# print(abs(T - T_q)/T_q)
 	return abs(T - T_q)/T_q
 
+def CC_experiment(m, inv_p, T):
+	MAX_TRY = 3*T				# 3 is arbitrary
 
-def find_best_config(T_q, gamma_q):
+	list_T = [MAX_TRY]*m
+	collected = [0]*m
+	num_collected = 0
+	for it in range(1,MAX_TRY+1):				
+		val = random.uniform(0,1)
+		coupon = math.floor(val*inv_p)
+		if coupon<m and collected[coupon] == 0:
+			collected[coupon] = 1
+			list_T[num_collected] = it
+			num_collected += 1
+			if num_collected == m:
+				break
+	return list_T
+
+
+def compute_error(m_q, inv_p_q, T_q, b):
+	list_error = [0]*m_q
+	list_exp = moment_CC(m_q, inv_p_q, 1)
+	if b==0:
+		for trial in range(NUM_TRIALS):
+			curr_list_T = CC_experiment(m_q, inv_p_q, T_q)
+			# print(curr_list_T)
+			for i in range(m_q):
+				list_error[i] = list_error[i] + relative_error(curr_list_T[i], T_q)
+		
+		bestind = 0
+		for i in range(m_q):
+			list_error[i] = list_error[i]/NUM_TRIALS
+			if relative_error(list_exp[i], T_q) > ERROR_TOLERANCE:
+				list_error[i] = math.inf
+			if i!=0:
+				if list_error[i] < list_error[bestind]:
+					list_error[bestind] = math.inf
+					bestind = i
+				else:
+					list_error[i] = math.inf	
+							
+
+	elif b==1:
+		list_sqvar = moment_CC(m_q, inv_p_q, 2)
+		for i in range(m_q):
+			list_error[i] = (list_exp[i]-T_q)*(list_exp[i]-T_q)  + list_sqvar[i]
+	
+	return list_error
+
+# This is forn iterating over powers of 2 for p
+# b=0: minimizes the |T-T_q|/T_q for all possible things within 5% expectation range 
+# actually run experiment and minimize average error
+# b=1: minimizes the E[(T-T_q)^2] = Var[T]+(E[T]-T_q)^2
+def find_best_config(T_q, gamma_q, b):
 	best_config = []
 	smallest_error = float('inf')
 	for i in range(P_LIMIT):
@@ -56,42 +100,39 @@ def find_best_config(T_q, gamma_q):
 		if upperbound_m < 1:
 			continue
 		for m_q in range(1,upperbound_m+1):
+			list_curr_error = compute_error(m_q, inv_p_q, T_q,b) 	# value for each n_q in {1,...,m_q} 
 			for n_q in range(1,m_q+1):
-				curr_expected_CC = expected_CC(m_q, inv_p_q, n_q)
-				curr_error = relative_error(curr_expected_CC, T_q)
+				curr_error = list_curr_error[n_q-1]
 				if curr_error < smallest_error:
 					best_config = [i, m_q, n_q]
 					smallest_error = curr_error
 
-	if smallest_error > ERROR_TOLERANCE:
-		print("Relative error greater than 0.05")
-	
+	# TODO: use function to compute mean relative error here
+	# if smallest_error > ERROR_TOLERANCE:
+	# 	print("Relative error greater than 0.05")
+	# print(smallest_error)
+
 	return best_config
 
-# # actually run experiment and minimize average error
-# def find_best_config_new(T_q, gamma_q):
-# 	best_config = []
-# 	smallest_error = float('inf')
-# 	for i in range(P_LIMIT):
-# 		inv_p_q = 2**i
-# 		upperbound_m = int(min(MAX_MEMORY, gamma_q*inv_p_q))
-# 		if upperbound_m < 1:
-# 			continue
-# 		for m_q in range(1,upperbound_m+1):
-# 			for n_q in range(1,m_q+1):
-# 				curr_error = 0
-# 				for trial in range(NUM_TRIALS):
-# 					curr_expected_CC = CC_experiment(m_q, inv_p_q, n_q, T_q)
-# 					curr_error += relative_error(curr_expected_CC, T_q)
-# 				curr_error = curr_error/NUM_TRIALS
-# 				if curr_error < smallest_error:
-# 					best_config = [i, m_q, n_q]
-# 					smallest_error = curr_error
 
-# 	if smallest_error > ERROR_TOLERANCE:
-# 		print("Relative error greater than 0.05")
-	
-	return best_config
+
+# Simulations for a single query :
+
+for T_q in range(1,123):
+	curr_best_config = find_best_config(T_q, 1, 0)
+	# print(curr_best_config)
+	val = curr_best_config[2]/curr_best_config[1]
+	print(val)
+
+	curr_best_config = find_best_config(T_q, 1, 1)
+	# print(curr_best_config)
+	val = curr_best_config[2]/curr_best_config[1]
+	print(val)
+	print()
+exit()
+
+
+# Running on queries_in.txt and packet_info.txt :
 
 fin = open("packet_info.txt", "r")
 packet_info = fin.read()
@@ -133,12 +174,13 @@ fout = open("queries.txt","w+")
 for i in range(number_queries):
 	query = queries_in[i].split(" ")
 	T_q = int(query[3])
-	curr_best_config = find_best_config(T_q, gamma_q)
+	curr_best_config = find_best_config(T_q, gamma_q, 1)
 	query[1] = subset_to_int(query[1])
 	query[2] = subset_to_int(query[2])
 	query = query+ curr_best_config
 	query = " ".join(str(x) for x in query)
 	fout.write(query+"\n")
 	# print(query)
-
 fout.close()
+
+
